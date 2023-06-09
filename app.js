177f8236d77fa1a4043f3b9b3b6793fba30a4192
daniel26jk <danielmoralese26@gmail.com>
2023-06-08 23:20:28 -0400

    listo

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/mostrar_mensaje', (req,res) =>{
     res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
 });
 
-app.get('/mostrar_sitio', (req,res) =>{
+app.get('/', (req,res) =>{
     res.sendFile((__dirname + '/views/index.html'));  // Permite enviar un archivo HTML
 });
 
